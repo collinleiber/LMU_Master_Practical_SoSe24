31acc167b7ec550613fdf6a5579ee5718cba16b9
OhItsLena <lena.hammerer@vol.at>
2024-05-02 21:34:19 +0200

    type hints

--- practical/ProcessMining/task2/alphaminer.py
+++ practical/ProcessMining/task2/alphaminer.py
@@ -1,3 +1,4 @@
+from typing import Dict, List, Tuple, Union
 import pandas as pd
 import numpy as np
 import pm4py
@@ -6,7 +7,7 @@ import os
 
 class AlphaMiner:
 
-    def __init__(self, file_path):
+    def __init__(self, file_path: str):
         self.event_log, self.activities, self.all_pairs = self._import_event_log(file_path)
         self.traces = self._extract_traces(self.event_log)
 
@@ -18,7 +19,7 @@ class AlphaMiner:
         self.not_following_pairs = self._get_not_following_pairs(self.following_pairs)
         self.before_pairs = self._get_before_pairs(self.not_following_pairs, self.sequential_pairs, self.parallel_pairs)
 
-    def footprint_matrix(self):
+    def footprint_matrix(self) -> pd.DataFrame:
         matrix = pd.DataFrame(index=sorted(self.activities.values()), columns=sorted(self.activities.values()))
 
         for pair in self.all_pairs:
@@ -37,7 +38,7 @@ class AlphaMiner:
 
         return matrix
 
-    def _import_event_log(self, file_path):
+    def _import_event_log(self, file_path: str) -> Tuple[pd.DataFrame, Dict[int, str], List[Tuple[int, int]]]:
         if not os.path.exists(file_path):
             raise Exception("File does not exist")
 
@@ -60,20 +61,20 @@ class AlphaMiner:
                                                                         activities.keys())).get(row['activity']), 1)
         return event_log, activities, all_pairs
 
-    def _create_alphabet(self, event_log):
+    def _create_alphabet(self, event_log: pd.DataFrame) -> Dict[int, str]:
         unique_activities = list(event_log['activity'].unique())
         activities = {i: unique_activities[i] for i in range(0, len(unique_activities))}
         return activities
 
-    def _extract_traces(self, event_log):
+    def _extract_traces(self, event_log: pd.DataFrame) -> np.ndarray:
         return np.asarray(event_log.groupby('case_id')['activity_id'].apply(np.asarray).values)
 
-    def _get_start_end_activities(self, traces):
+    def _get_start_end_activities(self, traces: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
         t_in = np.asarray(list(set([trace[0] for trace in traces])))
         t_out = np.asarray(list(set([trace[-1] for trace in traces])))
         return t_in, t_out
 
-    def _get_following_pairs(self, traces):
+    def _get_following_pairs(self, traces: np.ndarray) -> np.ndarray:
         pairs = []
         for trace in traces:
             for i in range(0, len(trace)-1):
@@ -82,7 +83,7 @@ class AlphaMiner:
         unique_pairs = np.asarray(list(set(pairs)))
         return unique_pairs
 
-    def _get_parallel_pairs(self, following_pairs):
+    def _get_parallel_pairs(self, following_pairs: np.ndarray) -> np.ndarray:
         parallel_pairs = []
         reversed_pairs = np.asarray([pair[::-1] for pair in following_pairs])
         for pair in following_pairs:
@@ -91,19 +92,20 @@ class AlphaMiner:
                     parallel_pairs.append(pair)
         return np.asarray(parallel_pairs)
 
-    def _get_sequential_pairs(self, following_pairs, parallel_pairs):
+    def _get_sequential_pairs(self, following_pairs: np.ndarray, parallel_pairs: np.ndarray) -> np.ndarray:
         sequential_pairs = np.asarray([pair for pair in following_pairs
                                        if not np.any(np.all(parallel_pairs == pair, axis=1))])
         return sequential_pairs
 
-    def _get_not_following_pairs(self, following_pairs):
+    def _get_not_following_pairs(self, following_pairs: np.ndarray) -> np.ndarray:
         reversed_pairs = np.asarray([pair[::-1] for pair in following_pairs])
         not_following_pairs = np.asarray([pair for pair in self.all_pairs
                                           if not np.any(np.all(following_pairs == pair, axis=1))
                                           and not np.any(np.all(reversed_pairs == pair, axis=1))])
         return not_following_pairs
 
-    def _get_before_pairs(self, not_following_pairs, sequential_pairs, parallel_pairs):
+    def _get_before_pairs(self, not_following_pairs: np.ndarray,
+                          sequential_pairs: np.ndarray, parallel_pairs: np.ndarray) -> np.ndarray:
         before_pairs = np.asarray([pair for pair in self.all_pairs
                                    if not np.any(np.all(not_following_pairs == pair, axis=1))
                                    and not np.any(np.all(sequential_pairs == pair, axis=1))

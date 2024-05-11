eb9b83ab9ec3d3c6b9120d6f33b5f9386b541a0f
Dilemmaqwer <158567553+Dilemmaqwer@users.noreply.github.com>
2024-05-11 20:14:11 +0200

    Real Data&Data Cleasing file
    real data links:
    https://data.4tu.nl/articles/_/12674816/1
    
    Since it is too big, so i would not upload it. falls test, put it in example_files folder.

---
+++ practical/ProcessMining/group1/task2/data-cleasing.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import os
+import io
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, 'example_files', 'BPI2016_Clicks_Logged_In.csv')
+
+# 读取CSV文件，指定分号为分隔符
+data = pd.read_csv(file_path, sep=';')
+# data = pd.read_csv(file_path, error_bad_lines=False, warn_bad_lines=True)
+
+# print(data.columns)
+
+data = data[['SessionID', 'TIMESTAMP', 'PAGE_NAME']]
+
+data.rename(columns={'SessionID': 'case_id', 'TIMESTAMP': 'timestamp', 'PAGE_NAME': 'activity'}, inplace=True)
+
+data.sort_values(by=['case_id', 'timestamp'], inplace=True)
+
+data_grouped = data.groupby('case_id').apply(lambda x: x.iloc[max(0, x.index[0] - 10000): min(x.index[-1] + 1001, len(x))])
+
+data_grouped.reset_index(drop=True, inplace=True)
+
+output_file_path = os.path.join(current_dir, 'sorted_session_data.csv')
+data_grouped.to_csv(output_file_path, index=False)
+
+print("Data was already cleaned and saved in:", output_file_path)

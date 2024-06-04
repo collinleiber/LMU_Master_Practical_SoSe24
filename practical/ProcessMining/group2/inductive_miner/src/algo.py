b5d2b33f0c1f5b947fad67b6f09aee87ccf80f8c
Mona <roessramona@gmail.com>
2024-06-05 00:01:52 +0200

    Implement exclusive choice cuts

--- practical/ProcessMining/group2/inductive_miner/src/algo.py
+++ practical/ProcessMining/group2/inductive_miner/src/algo.py
@@ -1,4 +1,5 @@
 import os
+from practical.ProcessMining.group2.inductive_miner.src.graph_utils import *
 
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,33 +19,31 @@ class EventLog:
         
     # TODO - other input methods?
 
-class DirectlyFollowsGraph:
+class DirectlyFollowsGraph(Graph):
     def __init__(self, event_log: EventLog):
         self.event_log = event_log
-        self.graph = {} # Adjacency list, format: "node_id" : [children]
+        self.graph = defaultdict(list) # Adjacency list, format: "node_id" : [children]
         self.start_nodes = set()
         self.end_nodes = set()
 
     def construct_dfg(self):
         for trace in self.event_log.traces.keys():
-            if trace:
-                self.start_nodes.add(trace[0])
-                self.end_nodes.add(trace[-1])
+            if trace: # Check if trace is not empty
+                self.start_nodes.add(trace[0]) # First activity in trace is a start
+                self.end_nodes.add(trace[-1]) # Last activity in trace is an end
 
                 for i in range(len(trace) - 1):
                     current_activity = trace[i]
                     next_activity = trace[i + 1]
 
-                    if current_activity not in self.graph:
-                        self.graph[current_activity] = []
                     if next_activity not in self.graph[current_activity]:
                         self.graph[current_activity].append(next_activity)
-                    if next_activity not in self.graph:
+                    if next_activity not in self.graph: # Add next activity to graph if not already present, i.e. if it is an end node
                         self.graph[next_activity] = []
 
     # Debugging helper
     def print_graph(self):
-        print("Graph: ", self.graph)
+        print("Graph: ", dict(self.graph))
         print("Start nodes: ", self.start_nodes)
         print("End nodes: ", self.end_nodes)
 
@@ -54,8 +53,27 @@ class ProcessTree:
         self.nodes = [] # TODO: Think about data structure for different types of nodes, dictionary sufficient?
         self.edges = [] # Format: (node1, node2)
 
-    def find_exclusive_choice_split(self, directly_follows_graph: DirectlyFollowsGraph):
-        pass
+    def find_exclusive_choice_split(self, dfg: DirectlyFollowsGraph):
+        # Find strongly connected components
+        components = dfg.find_components()
+
+        # Collapse the graph
+        scc_graph = dfg.build_scc_graph(components)
+        collapsed_graph = Graph(scc_graph)
+
+        # Find all pairs of nodes that are not reachable from each other
+        unreachable_pairs = collapsed_graph.find_non_reachable_pairs()
+
+        # Convert collpsed nodes back to components
+        exclusive_choice_cut_ids = set()
+        for pair in unreachable_pairs:
+            exclusive_choice_cut_ids.update(pair)
+
+        exclusive_choice_cuts = [components[id] for id in exclusive_choice_cut_ids]
+
+        print("Exclusive choice cuts: ", exclusive_choice_cuts)
+        return exclusive_choice_cuts
+
 
     def find_sequence_split(self, directly_follows_graph: DirectlyFollowsGraph):
         pass
@@ -67,7 +85,7 @@ class ProcessTree:
         pass
 
     def construct_process_tree(self, directly_follows_graph: DirectlyFollowsGraph):
-        pass
+        self.find_exclusive_choice_split(directly_follows_graph)
 
 class InductiveMiner():
     def __init__(self):
@@ -77,6 +95,7 @@ class InductiveMiner():
         # Step 1: Construct Directly-Follows Graph (DFG)
         dfg = DirectlyFollowsGraph(event_log)
         dfg.construct_dfg()
+        dfg.print_graph()
 
         # Step 2: Construct Process Tree from DFG
         process_tree = ProcessTree()
---
+++ practical/ProcessMining/group2/inductive_miner/src/graph_utils.py
@@ -0,0 +1,148 @@
+from typing import List, Optional
+from collections import defaultdict
+
+class Graph:
+    # TODO: Add type hints and clean up
+    def __init__(self, initial_graph: Optional[dict] = None):
+        if initial_graph is not None:
+            self.graph = initial_graph
+        else:
+            self.graph = defaultdict(list)  # Adjacency list, format: "node_id" : [children]
+
+    def add_edge(self, u, v) -> None:
+        self.graph[u].append(v)
+
+    def add_node(self, u) -> None:
+        if u not in self.graph:
+            self.graph[u] = []
+
+    def get_neighbors(self, u) -> list:
+        return self.graph[u]
+
+    def get_all_nodes(self) -> list:
+        return list(self.graph.keys())
+
+    def __str__(self) -> str:
+        result = ""
+        for node in self.graph:
+            result += f"{node} -> {self.graph[node]}\n"
+        return result
+
+    def is_reachable(self, node1, node2) -> bool:
+        def dfs(current, target, visited):
+            if current == target:
+                return True
+            visited.add(current)
+            for neighbor in self.get_neighbors(current):
+                if neighbor not in visited:
+                    if dfs(neighbor, target, visited):
+                        return True
+            return False
+        
+        return dfs(node1, node2, visited = set())
+
+    def find_components(self) -> List[List]:
+        # Kosaraju's Algorithm
+
+        def fill_stack(node):
+            # Get order for reverse dfs
+            visited.add(node)
+            for child in self.graph[node]:
+                if child not in visited:
+                    fill_stack(child)
+            stack.append(node) 
+
+        def reverse_graph(self) -> dict:
+            # Reverse all edges
+            reversed_graph = defaultdict(list)
+            for node in self.graph.keys():
+                for child in self.graph[node]:
+                    reversed_graph[child].append(node)
+            return reversed_graph
+
+        def dfs(graph, node, scc):
+            scc.append(node)
+            visited.add(node)
+            for child in graph[node]:
+                if child not in visited:
+                    dfs(graph, child, scc)
+
+        visited = set()
+        stack = []
+        sccs = [] # Strongly connected components
+
+        # 1. Apply DFS on original graph and find order of nodes
+        for node in self.graph.keys():
+            if node not in visited:
+                fill_stack(node) 
+
+        # 2. Reverse the graph
+        reversed_graph = reverse_graph(self)
+
+        # 3. Apply DFS on reversed graph in order of nodes from stack
+        visited = set()
+        while stack:
+            node = stack.pop()
+            if node not in visited:
+                scc = []
+                dfs(reversed_graph, node, scc)
+                sccs.append(scc)
+
+        return sccs
+    
+    def build_scc_graph(self, sccs):
+        scc_graph = defaultdict(set)
+        scc_map = {}
+
+        for i, scc in enumerate(sccs):
+            for node in scc:
+                scc_map[node] = i
+
+        for node in self.graph:
+            for neighbor in self.get_neighbors(node):
+                if scc_map[node] != scc_map[neighbor]:
+                    scc_graph[scc_map[node]].add(scc_map[neighbor])
+
+        return dict(scc_graph)
+    
+
+    def dfs_in_dag(self, start, visited):
+        stack = [start]
+        reachable = set()
+        while stack:
+            node = stack.pop()
+            if node not in visited:
+                visited.add(node)
+                reachable.add(node)
+                stack.extend(self.graph.get(node, [])) # TODO: maybe refactor a little bit
+        return reachable
+
+    def all_pairs_reachability_dag(self):
+        nodes = list(self.graph.keys())
+        reach = {node: set() for node in nodes}
+        
+        for node in nodes:
+            visited = set()
+            reach[node] = self.dfs_in_dag(node, visited)
+        
+        return reach
+
+    def find_non_reachable_pairs(self): # TODO: refactor
+        reach = self.all_pairs_reachability_dag()
+        nodes = list(self.graph.keys())
+        non_reachable_pairs = set()
+        
+        for i in range(len(nodes)):
+            for j in range(i + 1, len(nodes)):
+                u = nodes[i]
+                v = nodes[j]
+                if v not in reach[u] and u not in reach[v]:
+                    non_reachable_pairs.add((u, v))
+        
+        return list(non_reachable_pairs)
+
+
+# graph = {'a': ['b', 'c'], 'b': ['c', 'd', 'e'], 'c': ['d', 'b', 'e'], 'd': [], 'e': ['f'], 'f': ['c', 'b']}
+# g = Graph(graph)
+# scc = g.find_components()
+# print(scc)

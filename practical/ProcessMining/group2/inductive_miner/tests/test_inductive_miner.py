397d00699fc688b0e99e369d8d76af946fab7ed7
Mona <roessramona@gmail.com>
2024-06-05 00:04:05 +0200

    Init some ideas for tests

---
+++ practical/ProcessMining/group2/inductive_miner/tests/test_inductive_miner.py
@@ -0,0 +1,38 @@
+from practical.ProcessMining.group2.inductive_miner.src.algo import *
+
+def test_file_loading():
+    # TODO - think about different test cases, like empty file, file with only one trace, file with multiple traces
+    # Test if file is loaded correctly
+    pass
+
+def test_construct_dfg():
+    # Test if DFG is constructed correctly
+    pass
+
+def test_find_exclusive_choice_split():
+    # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
+    # Test if exclusive choice split is found correctly
+    pass
+
+def test_find_sequence_split():
+    # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
+    # Test if sequence split is found correctly
+    pass
+
+def test_find_parallel_split():
+    # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
+    # Test if parallel split is found correctly
+    pass
+
+def test_find_loop_split():
+    # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
+    # Test if loop split is found correctly
+    pass
+
+def test_construct_process_tree():
+    # Test if process tree is constructed correctly
+    pass
+
+def test_mine_process_model():
+    # Test if process model is mined correctly
+    pass

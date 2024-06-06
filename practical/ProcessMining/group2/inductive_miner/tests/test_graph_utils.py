ad2346427d67aec9465b73eca5502d8d09f5b100
Mona <roessramona@gmail.com>
2024-06-06 02:05:22 +0200

    Clean up testing mess, revert merge

--- practical/ProcessMining/group2/inductive_miner/tests/test_graph_utils.py
+++ practical/ProcessMining/group2/inductive_miner/tests/test_graph_utils.py
@@ -1,37 +1,66 @@
-from practical.ProcessMining.group2.inductive_miner.src.graph_utils import *
+from graph_utils import *
+
 
 def test_initialize_graph():
     # Test if graph is initialized correctly with and without input
-    pass
+    graph_no_input = Graph(None)
+    assert len(graph_no_input.graph) == 0
+    graph_with_input = Graph({'a': ['b'], 'b': ['c', 'd'], 'c': [], 'd': []})
+    assert len(graph_with_input.graph) > 0
+
 
 def test_add_edge():
     # Test if edge is added correctly
-    pass
+    g = Graph({'a': ['b'], 'b': ['c', 'd'], 'c': [], 'd': []})
+    g.add_edge('a', 'c')
+    assert g.graph == {'a': ['b', 'c'], 'b': ['c', 'd'], 'c': [], 'd': []}
+
 
 def test_add_node():
     # Test if node is added correctly
-    pass
+    g = Graph({'a': ['b'], 'b': ['c', 'd'], 'c': [], 'd': []})
+    g.add_node('e')
+    assert g.graph == {'a': ['b'], 'b': ['c', 'd'], 'c': [], 'd': [], 'e': []}
+
 
 def test_get_neighbors():
     # Test if neighbors are returned correctly
-    pass
+    g = Graph({'a': ['b'], 'b': ['c', 'd'], 'c': [], 'd': []})
+    assert g.get_neighbors('c') == []
+    assert g.get_neighbors('b') == ['c', 'd']
+
 
 def test_get_all_nodes():
     # Test if all nodes are returned correctly
-    pass
+    g = Graph({'a': ['b'], 'b': ['c', 'd'], 'c': [], 'd': []})
+    assert g.get_all_nodes() == ['a', 'b', 'c', 'd']
+
 
 def test_is_reachable():
     # Test if reachability is checked correctly
-    pass
+    g = Graph({'a': ['b'], 'b': ['c', 'd'], 'c': [], 'd': [], 'e': []})
+    assert g.is_reachable('a', 'c') == True
+    assert g.is_reachable('c', 'a') == False
+    assert g.is_reachable('a', 'e') == False
+
 
 def test_find_components():
     # Test if components are found correctly
-    pass
+    g = Graph({'a': ['b'], 'b': ['c', 'd'], 'c': [], 'd': []})
+    assert g.find_components() == [['a'], ['b'], ['d'], ['c']]
+
+
+def test_reverse_graph():
+    # Test if graph is reversed correctly
+    g = Graph({'a': ['b'], 'b': ['c', 'd'], 'c': [], 'd': [], 'e': []})
+    assert g.reverse_graph() == {'a': [], 'b': ['a'], 'c': ['b'], 'd': ['b'], 'e': []}
+
 
 def test_dfs():
     # Test if DFS is implemented correctly
     pass
 
-def test_reverse_graph():
-    # Test if graph is reversed correctly
+
+def test_find_non_reachable_pairs():
+    # Test if there are non reachable pairs
     pass
--- practical/ProcessMining/group2/inductive_miner/tests/test_inductive_miner.py
+++ practical/ProcessMining/group2/inductive_miner/tests/test_inductive_miner.py
@@ -1,37 +1,101 @@
-from practical.ProcessMining.group2.inductive_miner.src.algo import *
+from algo import *
+
 
 def test_file_loading():
-    # TODO - think about different test cases, like empty file, file with only one trace, file with multiple traces
+    # Tests about different test cases, like empty file, file with only one trace, file with multiple traces
     # Test if file is loaded correctly
-    pass
+    e0 = EventLog.from_file('./../data/log_empty.txt')
+    assert len(e0.traces) == 0
+
+    e1 = EventLog.from_file('./../data/log_one_trace.txt')
+    assert len(e1.traces) == 1
+    assert e1.traces == {
+        'acdeh': 1,
+    }
+
+    e2 = EventLog.from_file('./../data/log_from_paper.txt')
+    assert len(e2.traces) > 0
+    assert e2.traces == {
+        'acdeh': 1,
+        'abdeg': 1,
+        'adceh': 1,
+        'abdeh': 2,
+        'acdeg': 1,
+        'adceg': 1,
+        'acdefdbeh': 1,
+        'adbeg': 1,
+        'acdefbdeh': 1,
+        'acdefbdeg': 1,
+        'acdefdbeg': 1,
+        'adcefcdeh': 1,
+        'adcefdbeh': 1,
+        'adcefbdeg': 1,
+        'acdefbdefdbeg': 1,
+        'adcefdbeg': 1,
+        'adcefbdefbdeg': 1,
+        'adcefdbefbdeh': 1,
+        'adbefbdefdbeg': 1,
+        'adcefdbefcdefdbeg': 1,
+    }
+
 
 def test_construct_dfg():
     # Test if DFG is constructed correctly
-    pass
+    e0 = EventLog.from_traces({})
+    dfg0 = DirectlyFollowsGraph(e0)
+    dfg0.construct_dfg()
+    assert len(dfg0.graph) == 0
+    assert len(dfg0.start_nodes) == 0
+    assert len(dfg0.end_nodes) == 0
+
+    e1 = EventLog.from_traces({'abcd': 3, 'acbd': 2, 'aed': 1})
+    dfg1 = DirectlyFollowsGraph(e1)
+    dfg1.construct_dfg()
+    assert len(dfg1.graph) > 0
+    assert dfg1.graph == {
+        'a': ['b', 'c', 'e'],
+        'b': ['c', 'd'],
+        'c': ['d', 'b'],
+        'd': [],
+        'e': ['d'],
+    }
+    assert dfg1.start_nodes == {'a'}
+    assert dfg1.end_nodes == {'d'}
+
 
 def test_find_exclusive_choice_split():
     # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
     # Test if exclusive choice split is found correctly
-    pass
+    e0 = EventLog.from_traces({'abcd': 3, 'acbd': 2, 'aed': 1})
+    dfg0 = DirectlyFollowsGraph(e0)
+    dfg0.construct_dfg()
+    p0 = ProcessTree()
+    assert len(p0.find_exclusive_choice_split(dfg0)) > 0
+    assert p0.find_exclusive_choice_split(dfg0) == [['e'], ['b', 'c']]
+
 
 def test_find_sequence_split():
     # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
     # Test if sequence split is found correctly
     pass
 
+
 def test_find_parallel_split():
     # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
     # Test if parallel split is found correctly
     pass
+
 
 def test_find_loop_split():
     # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
     # Test if loop split is found correctly
     pass
 
+
 def test_construct_process_tree():
     # Test if process tree is constructed correctly
     pass
+
 
 def test_mine_process_model():
     # Test if process model is mined correctly

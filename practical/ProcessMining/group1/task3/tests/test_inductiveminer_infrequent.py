34de61dd87c6ee2e1e51b04f6bd5d9917a133d5b
Tim Jacob Edelmann <tim.edelmann@campus.lmu.de>
2024-06-06 16:54:51 +0200

    added empty unit tests

---
+++ practical/ProcessMining/group1/task3/tests/test_inductiveminer_infrequent.py
@@ -0,0 +1,12 @@
+
+
+class TestInductiveMinerInfrequent:
+    def test_get_frequent_directly_follows_graph(self):
+        pass
+
+    def test_calculate_eventually_follows_graph(self):
+        pass
+
+    def test_get_frequent_eventually_follows_graph(self):
+        pass
+

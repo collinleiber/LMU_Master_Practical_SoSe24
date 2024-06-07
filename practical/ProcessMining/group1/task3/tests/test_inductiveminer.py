bd77c187e6c5ad8d07ca49ccfc6dba701de8ec66
OhItsLena <lena.hammerer@vol.at>
2024-06-07 17:50:25 +0200

    fix fall through test

--- practical/ProcessMining/group1/task3/tests/test_inductiveminer.py
+++ practical/ProcessMining/group1/task3/tests/test_inductiveminer.py
@@ -132,7 +132,7 @@ class TestInductiveMiner:
         assert operator == CutType.LOOP
 
     def test_fall_through_flower_model(self):
-        log = [('a', 'b', 'c', 'd', 'e', 'f', 'g')]
+        log = [('a',), ('b',), ('c',), ('d',), ('e',), ('f',), ('g',)]
         miner = InductiveMiner(log)
         miner.run()
         assert miner.process_tree_str == f'{CutType.LOOP.value}({InductiveMiner.TAU}, a, b, c, d, e, f, g)'

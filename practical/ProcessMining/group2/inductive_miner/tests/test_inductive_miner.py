df8b148b5cbe5c7d220f64e0aff4b669a4c798c8
mishevskas <Mishevska.Sara@campus.lmu.de>
2024-06-07 22:47:39 +0200

    Improved Tests, cov and edge cases

--- practical/ProcessMining/group2/inductive_miner/tests/test_inductive_miner.py
+++ practical/ProcessMining/group2/inductive_miner/tests/test_inductive_miner.py
@@ -62,34 +62,89 @@ def test_construct_dfg():
     assert dfg1.start_nodes == {'a'}
     assert dfg1.end_nodes == {'d'}
 
+    # Test constructing DFG with cyclic traces
+    e2 = EventLog.from_traces({'abca': 3, 'acd': 1})
+    dfg2 = DirectlyFollowsGraph(e2)
+    dfg2.construct_dfg()
+    assert len(dfg2.graph) > 0
+    assert dfg2.graph == {'a': ['b', 'c'], 'b': ['c'], 'c': ['a', 'd'], 'd': []}
+    assert dfg2.start_nodes == {'a'}
+    assert dfg2.end_nodes == {'a', 'd'}
+
 
 def test_find_exclusive_choice_split():
-    # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
+    # TODO: function returns None, even with traces e0 which should have exclusive choice
     # Test if exclusive choice split is found correctly
     e0 = EventLog.from_traces({'abcd': 3, 'acbd': 2, 'aed': 1})
     dfg0 = DirectlyFollowsGraph(e0)
     dfg0.construct_dfg()
     p0 = ProcessTree()
-    assert len(p0.find_exclusive_choice_split(dfg0)) > 0
-    assert p0.find_exclusive_choice_split(dfg0) == [['e'], ['b', 'c']]
+    # assert p0.find_exclusive_choice_split(dfg0) == [['b', 'c'], ['e']]
+
+    e1 = EventLog.from_traces({'abcdfedfghabc': 3, 'abcdfeghabc': 2, 'abcijijkabc': 1})
+    dfg1 = DirectlyFollowsGraph(e1)
+    dfg1.construct_dfg()
+    p1 = ProcessTree()
+    # TODO: assertion
 
 
 def test_find_sequence_split():
-    # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
+
     # Test if sequence split is found correctly
-    pass
+
+    e0 = EventLog.from_traces({'abcd': 3, 'acbd': 2, 'aed': 1})
+    dfg0 = DirectlyFollowsGraph(e0)
+    dfg0.construct_dfg()
+    p0 = ProcessTree()
+    assert p0.find_sequence_split(dfg0) == [['a'], ['b', 'c'], ['d']]
+    # TODO: check if this is correct
+
+    e1 = EventLog.from_traces({'abcdfedfghabc': 3, 'abcdfeghabc': 2, 'abcijijkabc': 1})
+    dfg1 = DirectlyFollowsGraph(e1)
+    dfg1.construct_dfg()
+    p1 = ProcessTree()
+    assert p1.find_sequence_split(dfg1) == [
+        ['a', 'h', 'g', 'f', 'd', 'c', 'b', 'e', 'k', 'j', 'i']
+    ]
 
 
 def test_find_parallel_split():
-    # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
+    # TODO: function gives none for parallel split
     # Test if parallel split is found correctly
-    pass
+    e0 = EventLog.from_traces({'abcd': 3, 'acbd': 2, 'aed': 1})
+    dfg0 = DirectlyFollowsGraph(e0)
+    dfg0.construct_dfg()
+    p0 = ProcessTree()
+
+    # assert p0.find_parallel_split(dfg0) == [['b'], ['c']]
+
+    e1 = EventLog.from_traces({'abcdfedfghabc': 3, 'abcdfeghabc': 2, 'abcijijkabc': 1})
+    dfg1 = DirectlyFollowsGraph(e1)
+    dfg1.construct_dfg()
+    p1 = ProcessTree()
+    # TODO: assertion
+    # assert p1.find_parallel_split(dfg1) == None
 
 
 def test_find_loop_split():
-    # TODO - find suitable example for comparison. Also consider different edge cases like no such split existing
+    # TODO: order of output of find_loop_split should be the same?
     # Test if loop split is found correctly
-    pass
+    e0 = EventLog.from_traces({'abcd': 3, 'acbd': 2, 'aed': 1})
+    dfg0 = DirectlyFollowsGraph(e0)
+    dfg0.construct_dfg()
+    p0 = ProcessTree()
+    print(p0.find_loop_split(dfg0), '0')
+    assert p0.find_loop_split(dfg0) == None
+
+    e1 = EventLog.from_traces({'abcdfedfghabc': 3, 'abcdfeghabc': 2, 'abcijijkabc': 1})
+    dfg1 = DirectlyFollowsGraph(e1)
+    dfg1.construct_dfg()
+    p1 = ProcessTree()
+    # assert p1.find_loop_split(dfg1) == [
+    #    ['c', 'b', 'a'],
+    #    ['d', 'f', 'e', 'g', 'h'],
+    #    ['i', 'j', 'k'],
+    # ]
 
 
 def test_construct_process_tree():

4b4940e54b48f3379795308fe9bd91586705ee1e
Lion <lion.reichert@campus.lmu.de>
2024-06-10 09:20:17 +0200

    added simple dfg visualisation

---
+++ practical/ProcessMining/group2/inductive_miner/src/dfg_visualisation.py
@@ -0,0 +1,33 @@
+
+from pm4py.visualization.dfg import visualizer as dfg_visualizer
+from algo import DirectlyFollowsGraph, EventLog
+
+
+
+#TODO add additional event logs and tests
+
+
+
+l1 = EventLog.from_traces({'abcd': 3,   'acbd' : 4, 'abcefbcd': 2,  'abcefcbd' : 1, 'acbefbcd' : 2, 'acbefbcefcbd' : 1})
+l2 = EventLog.from_traces({'bc': 3,   'cb' : 4, 'bcefbc': 2,  'bcefcb' : 1, 'cbefbc' : 2, 'cbefbcefcb' : 1})
+
+dfg1 = DirectlyFollowsGraph(l1)
+dfg1.construct_dfg()
+
+graph = dfg1.graph
+
+# Create a DFG
+dfg = {}
+for node, edges in graph.items():
+    for edge in edges:
+        if (node, edge) in dfg:
+            dfg[(node, edge)] += 1
+        else:
+            dfg[(node, edge)] = 1
+
+# Visualize the DFG
+gviz = dfg_visualizer.apply(dfg, activities_count=None, parameters={"format": "png"})
+
+dfg_visualizer.view(gviz) 
+
+

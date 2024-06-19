dd1ef1efa9b1659119640d5a6fc07a3fecd8a1e7
Lion <lion.reichert@campus.lmu.de>
2024-06-19 22:34:46 +0200

    added visualisation to the footprint matrix

--- practical/ProcessMining/group2/conformance_checking/src/generate_footprint.py
+++ practical/ProcessMining/group2/conformance_checking/src/generate_footprint.py
@@ -2,7 +2,7 @@ from sortedcontainers import SortedSet, SortedDict
 from itertools import chain
 import numpy as np
 from enum import Enum
-
+import matplotlib.pyplot as plt
 class Relations(Enum):
     BEFORE = '<-'
     SEQUENCE =  '->'   
@@ -73,9 +73,38 @@ class Conformance_checking:
                     all_relations = Relations.NOT_FOLLOWED.value
                 self.relations[transition_1][transition_2] = all_relations
 
+    def visualize_sorted_dict(self, sorted_dict):
+        keys = list(sorted_dict.keys())
+        size = len(keys)
+        
+        
+
+        fig, ax = plt.subplots(figsize=(7, 7)) 
+    
+        ax.matshow([[0] * size] * size, cmap='Greys')  # Create an empty heatmap
+
+        # Add gridlines
+        ax.set_xticks([x - 0.5 for x in range(1, size)], minor=True)
+        ax.set_yticks([y - 0.5 for y in range(1, size)], minor=True)
+        ax.grid(which='minor', color='black', linestyle='-', linewidth=5)
+        for i, row_key in enumerate(keys):
+            for j, col_key in enumerate(keys):
+                ax.text(j, i, sorted_dict[row_key][col_key], ha='center', va='center', color='black', fontsize=38)
+
+        ax.set_xticks(range(size))
+        ax.set_yticks(range(size))
+        ax.set_xticklabels(keys, fontsize=28)
+        ax.set_yticklabels(keys, fontsize=28)
+        ax.tick_params(axis='both', which='major', labelsize=28)
+        for spine in ax.spines.values():
+            spine.set_edgecolor('black')
+            spine.set_linewidth(5)  # Adjust the linewidth for the outer border
+        plt.title('Footprint Matrix', fontsize=22)
+        plt.show()
 
 #TODO add different conformance checking variations
 #TODO implement comparison
+#TODO add comments
 
 
 

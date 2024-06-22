f5bfc2b838edc4909849d29c1c348bf336fd8d5e
OhItsLena <lena.hammerer@vol.at>
2024-06-22 12:40:35 +0200

    Use visualizer in IM

--- practical/ProcessMining/group1/shared/visualizer.py
+++ practical/ProcessMining/group1/shared/visualizer.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from IPython.core.display import Image
 from IPython.core.display_functions import display
@@ -9,24 +9,46 @@ import tempfile
 
 class Visualizer:
 
-    def __init__(self, net: PetriNet, initial_marking: Marking, final_marking: Marking, tokens: Dict):
-        self.net = net
-        self.initial_marking = initial_marking
-        self.final_marking = final_marking
-        self.tokens = tokens
-
-    def get_petri_net(self) -> Digraph:
+    def get_process_tree(self, tree, graph=None) -> Digraph:
         filename = tempfile.NamedTemporaryFile(suffix='.gv')
         filename.close()
 
-        graph = Digraph(self.net.name, filename=filename.name, engine='dot')
-        graph.graph_attr.update({'rankdir': 'LR', 'bgcolor': 'white', 'fontname': "Comic Sans MS"})
+        graph = graph if graph else Digraph("process tree", filename=filename.name, engine='dot')
         graph.format = 'png'
-        graph.fontname = "Comic Sans MS"
+
+        if isinstance(tree, tuple):
+            operator, children, node_id = tree
+            graph.node(node_id, operator, shape='circle')  # Create a circular node for the operator
+            for child in children:
+                if isinstance(child, tuple):
+                    child_id = child[2]
+                    self.get_process_tree(child, graph)  # Recursively add child nodes
+                else:
+                    # Create a unique ID for each τ
+                    if child == '𝜏':
+                        child_id = f'node{child}_{id(child)}'
+                    else:
+                        child_id = f'node{child}'
+                    graph.node(child_id, child, shape='box',
+                               fontname="Arial")  # Create a rectangular node for the activity
+                graph.edge(node_id, child_id, arrowhead='none')  # Add an edge from the operator to the child node
+        else:
+            node_id = f'node{tree}'
+            graph.node(node_id, tree, shape='box')  # Create a rectangular node for the activity
+        return graph
+
+    def get_petri_net(self, net: PetriNet, initial_marking: Marking, final_marking: Marking,
+                      tokens: Optional[Dict[str, Dict]] = None) -> Digraph:
+        filename = tempfile.NamedTemporaryFile(suffix='.gv')
+        filename.close()
+
+        graph = Digraph(net.name, filename=filename.name, engine='dot')
+        graph.graph_attr.update({'rankdir': 'LR', 'bgcolor': 'white'})
+        graph.format = 'png'
         font_size = "12"
 
         # transitions
-        for t in self.net.transitions:
+        for t in net.transitions:
             textcolor = "black"
             label = str(t.label)
             fillcolor = "transparent" if t.label else "black"
@@ -34,15 +56,16 @@ class Visualizer:
                                     fontsize=font_size, fontname="Arial", fontcolor=textcolor)
 
         # places
-        places_sort_list = sorted(list(self.net.places), key=lambda x: x.name)
+        places_sort_list = sorted(list(net.places), key=lambda x: x.name)
         for p in places_sort_list:
-            fillcolor, label, penwidth, fontsize, shape = self._get_place_attributes(p)
+            fillcolor, label, penwidth, fontsize, shape = self._get_place_attributes(p, initial_marking,
+                                                                                     final_marking, tokens)
             self._create_graph_node(graph, id(p), label, shape=shape, fixedsize='true', width='0.75', style="filled",
                                     fillcolor=fillcolor, fontname="Arial", weight="bold", penwidth=penwidth,
                                     fontsize=fontsize)
 
         # arcs
-        arcs_sort_list = sorted(list(self.net.arcs), key=lambda x: (x.source.name, x.target.name))
+        arcs_sort_list = sorted(list(net.arcs), key=lambda x: (x.source.name, x.target.name))
         for a in arcs_sort_list:
             self._create_graph_edge(graph, id(a.source), id(a.target), fontsize=font_size, arrowhead="normal")
 
@@ -58,15 +81,15 @@ class Visualizer:
     def _create_graph_edge(graph, source_id, target_id, **kwargs):
         graph.edge(str(source_id), str(target_id), **kwargs)
 
-    def _get_place_attributes(self, place):
+    @staticmethod
+    def _get_place_attributes(place, initial_marking: Marking, final_marking: Marking,
+                              tokens: Optional[Dict[str, Dict]] = None) -> tuple:
         fillcolor = "transparent"
         label = ""
         penwidth = "1"
         fontsize = "12"
         shape = "circle"
 
-        initial_marking = self.initial_marking
-        final_marking = self.final_marking
         if place in initial_marking:
             label = "<&#9679;>" if initial_marking[place] == 1 else str(initial_marking[place])
             fontsize = "34"
@@ -76,9 +99,9 @@ class Visualizer:
             fontsize = "32"
             shape = "doublecircle"
 
-        if self.tokens:
-            missing_tokens = self.tokens["missing"]
-            remaining_tokens = self.tokens["remaining"]
+        if tokens:
+            missing_tokens = tokens["missing"]
+            remaining_tokens = tokens["remaining"]
             if place in remaining_tokens and place in missing_tokens:
                 fillcolor = "plum"
                 label = f'<<B>+{remaining_tokens[place]}<BR></BR>-{missing_tokens[place]}</B>>'

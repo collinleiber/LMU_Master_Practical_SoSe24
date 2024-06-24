1e5b5bd52d230c85766cd4bc59732ba25ecf6154
Tim Jacob Edelmann <tim.edelmann@campus.lmu.de>
2024-06-24 19:46:57 +0200

    implementation for fitness calculation and empty simplicity calc

--- practical/ProcessMining/group1/task4/tokenreplay.py
+++ practical/ProcessMining/group1/task4/tokenreplay.py
@@ -113,42 +113,15 @@ class TokenReplay:
             'missing_tokens': self.missing_tokens,
             'remaining_tokens': self.remaining_tokens
         }
-from collections import defaultdict
 
-import pm4py
-
-
-class TokenReplay:
-    # Generic nets to provide the token replay algorithm for log2log, log2model, and model2model
-    def __init__(self, event_log, model_net):
-        self.event_log = event_log
-        self.model_net = model_net
-        self.fitness, self.simplicity = self.replay()
-
-    def replay(self, traces):
-        # Implement the token replay algorithm
-        result = []
-        for trace in traces:
-            produced, consumed, missing, remaining = 0, 0, 0, 0
-
-            # TODO replay the trace
-
-            result.append((produced, consumed, missing, remaining))
-
-        fitness = self.get_fitness(result)
-        simplicity = self.get_simplicity()
-
-    def get_fitness(self, token_results) -> float:
-        kpi = defaultdict(int)
-
-        for produced, consumed, missing, remaining in token_results:
-            kpi['p'] += produced
-            kpi['c'] += consumed
-            kpi['m'] += missing
-            kpi['r'] += remaining
-
-        fitness = 0.5 * (1 - (kpi['m'] / (kpi['c']) + 0.5 * (kpi['r'] / kpi['p'])))
+    def get_fitness(self) -> float:
+        fitness = (0.5 * (1 - (self.missing_tokens / self.consumed_tokens)) +
+                   0.5 * (1 - (self.remaining_tokens / self.produced_tokens)))
         return fitness
+
+    def get_simplicity(self):
+        pass
+
     def get_fitness_pm4py(self, log, net, init_marking, final_marking):
         return pm4py.conformance.fitness_token_based_replay(log, net, init_marking, final_marking)
 

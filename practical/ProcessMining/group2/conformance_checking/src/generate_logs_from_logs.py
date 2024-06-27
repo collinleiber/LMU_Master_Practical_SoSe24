6833bb8e1e56627a83ad6702705ddb73ab1399a3
mishevskas <Mishevska.Sara@campus.lmu.de>
2024-06-27 21:02:44 +0200

    Added the first 5 logs for testing

--- practical/ProcessMining/group2/conformance_checking/src/generate_logs_from_logs.py
+++ practical/ProcessMining/group2/conformance_checking/src/generate_logs_from_logs.py
@@ -2,10 +2,10 @@ from models_from_pm4py import get_model_from_pm4py, AlgoPm4Py
 from pm4py.visualization.petri_net import visualizer as pn_visualizer
 from models_from_pm4py import get_model_from_pm4py
 from replay import get_traces_with_replay
-import csv
 
-input_log_path = "./../InputLogs/L1.csv"
-output_log_path = "./../OutputLogs/L1.csv"
+log_name = "L5"
+input_log_path = "./../InputLogs/{}.csv".format(log_name)
+output_log_path = "./../OutputLogs/{}.csv".format(log_name)
 
 net, start, end = get_model_from_pm4py(input_log_path, AlgoPm4Py.ALPHAPLUS)
 # gviz = pn_visualizer.apply(net, start, end)
@@ -15,5 +15,5 @@ traces = get_traces_with_replay(net, start, end, len(net.transitions) * 2)
 with open(output_log_path, 'w') as file:
     for id, trace in enumerate(traces):
         for activity in trace:
-            file.write("{}, {}".format(id + 1, activity))
+            file.write("{},{}".format(id + 1, activity))
             file.write('\n')

c699ea430fe1224489737c5ceab274cdd38d43f4
Lion <lion.reichert@campus.lmu.de>
2024-06-30 17:25:46 +0200

    Check for csv or xes input in get_model_from_pm4py

--- practical/ProcessMining/group2/conformance_checking/src/models_from_pm4py.py
+++ practical/ProcessMining/group2/conformance_checking/src/models_from_pm4py.py
@@ -3,8 +3,7 @@ import os
 import pm4py
 from pm4py.objects.log.obj import EventLog, Trace, Event
 from enum import Enum
-
-
+from datetime import datetime
 FILE_DIR = os.path.dirname(__file__)
 
 class AlgoPm4Py(Enum):
@@ -13,32 +12,43 @@ class AlgoPm4Py(Enum):
     INDUCTIVEMINER = 3
     HEURISTICMINER = 4
 
-
 def get_model_from_pm4py(
-    file_path = os.path.join(FILE_DIR, "..", "Logs/L1.csv"), 
+    file_path=os.path.join(FILE_DIR, "..", "Logs/L1.csv"), 
     algorithm: AlgoPm4Py = AlgoPm4Py.ALPHAPLUS
 ):
     log = EventLog()
+    filetype = os.path.splitext(file_path)[1]
+    
+    if filetype == '.csv':
+        with open(file_path, newline='') as file:
+            reader = csv.reader(file)
+            current_trace = None
+            trace = None
+            for trace_id, activity in reader:
+                if current_trace != trace_id:
+                    if trace is not None:
+                        log.append(trace)
+                    trace = Trace()
+                    trace.attributes['concept:name'] = trace_id
+                    current_trace = trace_id
+                event = Event()
+                event["concept:name"] = activity
+                trace.append(event)
 
-    with open(file_path, newline='') as file:
-        reader = csv.reader(file)
-        current_trace = None
-        trace = None
-        for trace_id, activity in reader:
-            if current_trace != trace_id:
-                if trace is not None:
-                    log.append(trace)
-                trace = Trace()
-                trace.attributes['concept:name'] = trace_id
-                current_trace = trace_id
-            event = Event()
-            event["concept:name"] = activity
-            trace.append(event)
+            if trace is not None:
+                log.append(trace)
+        
+    elif filetype == '.xes': 
+        dataframe = pm4py.read_xes(file_path)
+        # Adding timestamps for conformity with pm4py
+        dataframe['time:timestamp'] = datetime.now()
+        dataframe = pm4py.format_dataframe(dataframe, case_id='case:concept:name', activity_key='concept:name', timestamp_key='time:timestamp')
+        log = pm4py.convert_to_event_log(dataframe)
 
-        if trace is not None:
-            log.append(trace)
     if algorithm == AlgoPm4Py.ALPHA:
+        
         return pm4py.discover_petri_net_alpha(log)
+    
     elif algorithm == AlgoPm4Py.ALPHAPLUS:
         return pm4py.discover_petri_net_alpha_plus(log)
     elif algorithm == AlgoPm4Py.INDUCTIVEMINER:

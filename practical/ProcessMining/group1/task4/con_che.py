d7991963d3c8aacd5362015f760a2f2528f2341a
Dilemmaqwer <158567553+Dilemmaqwer@users.noreply.github.com>
2024-06-30 15:14:57 +0200

    update visualizer part

--- practical/ProcessMining/group1/task4/con_che.py
+++ practical/ProcessMining/group1/task4/con_che.py
@@ -7,9 +7,30 @@ from pm4py.objects.conversion.process_tree import converter as pt_converter
 import pandas as pd
 from tokenreplay import TokenReplay
 from comparison import ModelComparator
+from pm4py.objects.process_tree.obj import ProcessTree
+from pm4py.visualization.process_tree import visualizer as pt_visualizer
+from pm4py.objects.conversion.log import converter as log_converter
+from practical.ProcessMining.group1.shared import utils
+from pm4py.visualization.petri_net import visualizer as pn_vis
+from pm4py.algo.discovery.inductive import algorithm as inductive_miner
+from pm4py.visualization.process_tree import visualizer as pt_vis
+from pm4py.objects.conversion.process_tree import converter as pt_to_petri_converter
+from practical.ProcessMining.group1.shared.visualizer import Visualizer
+from pm4py.algo.conformance.tokenreplay import algorithm as token_replay
+from practical.ProcessMining.group1.task2.alphaminer import AlphaMiner
+from graphviz import Digraph
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import os
+from practical.ProcessMining.group1.shared.utils import event_log_to_dataframe, read_txt_test_logs
+
+
+# ================== logs ================
 
 file_path = '../shared/example_files/DomesticDeclarations_afterDC.csv'
 # file_path = '../shared/example_files/common-example.csv'
+# file_path = '../shared/example_files/limitation-example1.csv'
+# file_path = '../shared/example_files/limitation-example1.csv'
 
 # Load the CSV file into a pandas DataFrame
 log = pd.read_csv(file_path, sep=';')
@@ -17,9 +38,64 @@ log['timestamp'] = pd.to_datetime(log['timestamp'], utc=True)
 event_log = pm4py.format_dataframe(log, case_id='case_id', activity_key='activity', timestamp_key='timestamp')
 event_log = log_converter.apply(event_log)
 
+# logs = read_txt_test_logs("../shared/example_files/simple_event_logs_modified.txt")
+# log = logs['L1']
+# log = event_log_to_dataframe(log)
+# log = pm4py.format_dataframe(log, case_id='case_id', activity_key='activity', timestamp_key='timestamp')
+# event_log = log_converter.apply(log)
+
+
+# ================== Miners ================
+
+# # alpha Miner
+alpha_net, alpha_initial_marking, alpha_final_marking = alpha_miner.apply(event_log)
+gviz_alpha = pn_vis.apply(alpha_net, alpha_initial_marking, alpha_final_marking)
+pm4py.visualization.petri_net.visualizer.view(gviz_alpha)
+
+# replayed_traces = token_replay.apply(log, alpha_net, alpha_initial_marking, alpha_initial_marking)
+# places = alpha_net.places
+# vizard.display(gviz_alpha)
+# graph_fitness = vizard.get_petri_net(alpha_net, alpha_initial_marking, alpha_initial_marking, replayed_traces)
+# vizard.display(graph_fitness)
+
+# Heuristic Miner
+heuristics_net, heuristics_initial_marking, heuristics_final_marking = heuristics_miner.apply(event_log)
+gviz_heuristics = pn_vis.apply(heuristics_net, heuristics_initial_marking, heuristics_final_marking)
+pm4py.visualization.petri_net.visualizer.view(gviz_heuristics)
+
+# IM
+process_tree_IM = inductive_miner.apply(log, variant=inductive_miner.Variants.IM)
+# gviz_tree_IM = pt_vis.apply(process_tree_IM)
+# pt_vis.view(gviz_tree_IM)
+net, initial_marking, final_marking = pt_to_petri_converter.apply(process_tree_IM)
+gviz_petri_IM = pn_vis.apply(net, initial_marking, final_marking)
+pn_vis.view(gviz_petri_IM)
+
+parameters = {
+    'noise_threshold': 0.2
+}
+# IMf
+process_tree_IMf = inductive_miner.apply(log, parameters=parameters,variant=inductive_miner.Variants.IMf)
+# gviz_tree_IMf = pt_visualizer.apply(process_tree_IMf)
+# pt_visualizer.view(gviz_tree_IMf)
+net, initial_marking, final_marking = pt_to_petri_converter.apply(process_tree_IMf)
+gviz_petri_IMf = pn_vis.apply(net, initial_marking, final_marking)
+pn_vis.view(gviz_petri_IMf)
+
+# ================== Token Replay ================
+
 # Alpha Miner
 alpha_net, alpha_initial_marking, alpha_final_marking = alpha_miner.apply(event_log)
 alpha_token_replay = TokenReplay(event_log, alpha_net, alpha_initial_marking, alpha_final_marking, "Alpha Miner")
+# print('al_token_replay ======', alpha_token_replay)
+vizard = Visualizer()
+places = alpha_net.places
+print('places:', places)
+tokens = alpha_token_replay.get_tokens()
+print('tokens ==========', tokens)
+graph_fitness = vizard.build_petri_net(alpha_net, alpha_initial_marking, alpha_final_marking, tokens)
+print('graph_fitness ===', graph_fitness)
+vizard.save(graph_fitness,'graph_fitness')
 
 # Inductive Miner
 inductive_tree = inductive_miner.apply(event_log)
@@ -27,9 +103,9 @@ inductive_net, inductive_initial_marking, inductive_final_marking = pt_converter
 inductive_token_replay = TokenReplay(event_log, inductive_net, inductive_initial_marking, inductive_final_marking, "Inductive Miner")
 
 # Inductive Miner Infrequent
-inductive_infreq_tree = inductive_miner.apply(event_log, variant=inductive_miner.Variants.IMf)
-inductive_infreq_net, inductive_infreq_initial_marking, inductive_infreq_final_marking = pt_converter.apply(inductive_infreq_tree)
-inductive_infreq_token_replay = TokenReplay(event_log, inductive_infreq_net, inductive_infreq_initial_marking, inductive_infreq_final_marking, "Inductive Miner Infrequent")
+inductive_inf_tree = inductive_miner.apply(event_log, variant=inductive_miner.Variants.IMf)
+inductive_inf_net, inductive_inf_initial_marking, inductive_inf_final_marking = pt_converter.apply(inductive_inf_tree)
+inductive_inf_token_replay = TokenReplay(event_log, inductive_inf_net, inductive_inf_initial_marking, inductive_inf_final_marking, "Inductive Miner Infrequent")
 
 
 # Heuristic Miner
@@ -38,7 +114,7 @@ heuristics_token_replay = TokenReplay(event_log, heuristics_net, heuristics_init
                                       heuristics_final_marking, "Heuristic Miner")
 
 # list of TokenReplay instances
-model_list = [alpha_token_replay, inductive_token_replay, inductive_infreq_token_replay, heuristics_token_replay]
+model_list = [alpha_token_replay, inductive_token_replay, inductive_inf_token_replay, heuristics_token_replay]
 
 # Instantiate
 comparator = ModelComparator(model_list)

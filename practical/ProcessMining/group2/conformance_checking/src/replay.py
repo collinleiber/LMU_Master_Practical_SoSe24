61786f799e985c0e2e20ba77f79d81937e64941c
mishevskas <Mishevska.Sara@campus.lmu.de>
2024-07-01 13:58:01 +0200

    updated replay

--- practical/ProcessMining/group2/conformance_checking/src/replay.py
+++ practical/ProcessMining/group2/conformance_checking/src/replay.py
@@ -1,13 +1,13 @@
-from pm4py.algo.simulation.playout.petri_net import algorithm
 import imported_pm4py.extensive as extensive
-from pm4py.objects.petri_net.semantics import is_enabled, execute
-from collections import deque
-from pm4py.objects.petri_net.obj import PetriNet
-from pm4py.objects.log.obj import Trace, Event, EventLog
-from pm4py.algo.simulation.playout.petri_net import algorithm
+import imported_pm4py.basic_playout as basic
+
+from pm4py.algo.simulation.playout.petri_net import variants
 
 
-def get_traces_with_replay(net, start, end, variant):
+def get_traces_with_replay(net, start, end, variant=variants.extensive):
     print("Replaying Petri Net with TQDM")
-    playout = extensive.apply(net, start, end)
+    if variant == variants.basic_playout:
+        playout = basic.apply(net, start, end)
+    else:
+        playout = extensive.apply(net, start, end)
     return playout

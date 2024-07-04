7675a57709a2045440938c0df018794da47a3550
Lion <lion.reichert@campus.lmu.de>
2024-07-04 22:48:37 +0200

    reverted some merge issue

--- practical/ProcessMining/group2/conformance_checking/src/models_from_pm4py.py
+++ practical/ProcessMining/group2/conformance_checking/src/models_from_pm4py.py
@@ -5,7 +5,6 @@ from pm4py.objects.log.obj import EventLog, Trace, Event
 from enum import Enum
 from pm4py.objects.log.importer.xes import importer
 
-from datetime import datetime
 FILE_DIR = os.path.dirname(__file__)
 
 
@@ -62,11 +61,9 @@ def get_log_from_file(file_path="InputLogs/L1.csv"):
     return log
 
 
-
-    
 def get_model_from_pm4py(
-    file_path=os.path.join(FILE_DIR, "..", "Logs/L1.csv"), 
-    algorithm: AlgoPm4Py = AlgoPm4Py.ALPHAPLUS
+    log,
+    algorithm: AlgoPm4Py = AlgoPm4Py.ALPHAPLUS,
 ):
     """
     Discover a Petri net model from an event log using the specified algorithm.
@@ -86,39 +83,8 @@ def get_model_from_pm4py(
     tuple
         A tuple containing the discovered Petri net, initial marking, and final marking.
     """
-    log = EventLog()
-    filetype = os.path.splitext(file_path)[1]
-    
-    if filetype == '.csv':
-        with open(file_path, newline='') as file:
-            reader = csv.reader(file)
-            current_trace = None
-            trace = None
-            for trace_id, activity in reader:
-                if current_trace != trace_id:
-                    if trace is not None:
-                        log.append(trace)
-                    trace = Trace()
-                    trace.attributes['concept:name'] = trace_id
-                    current_trace = trace_id
-                event = Event()
-                event["concept:name"] = activity
-                trace.append(event)
-
-            if trace is not None:
-                log.append(trace)
-        
-    elif filetype == '.xes': 
-        dataframe = pm4py.read_xes(file_path)
-        # Adding timestamps for conformity with pm4py
-        dataframe['time:timestamp'] = datetime.now()
-        dataframe = pm4py.format_dataframe(dataframe, case_id='case:concept:name', activity_key='concept:name', timestamp_key='time:timestamp')
-        log = pm4py.convert_to_event_log(dataframe)
-
     if algorithm == AlgoPm4Py.ALPHA:
-        
         return pm4py.discover_petri_net_alpha(log)
-    
     elif algorithm == AlgoPm4Py.ALPHAPLUS:
         return pm4py.discover_petri_net_alpha_plus(log)
     elif algorithm == AlgoPm4Py.INDUCTIVEMINER:

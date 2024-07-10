e7aeb0b3c4edb0c3a58ed42561459be20a152c09
Tim Jacob Edelmann <tim.edelmann@campus.lmu.de>
2024-07-10 22:56:57 +0200

    moved generic df transforming methods to utils

--- practical/ProcessMining/group1/shared/utils.py
+++ practical/ProcessMining/group1/shared/utils.py
@@ -1,5 +1,7 @@
 import csv
 import os
+
+import numpy as np
 import pandas as pd
 import pm4py
 import shutil
@@ -93,3 +95,47 @@ def check_lists_of_sets_equal(list1, list2):
     sorted_list2 = sorted([tuple(sorted(s)) for s in list2])
 
     return sorted_list1 == sorted_list2
+
+
+def filter_rare_traces(log, threshold: int = 2, case_column='case:concept:name'):
+    """ Filters out traces that occur less than the given threshold in absolut. """
+    case_counts = log.groupby(case_column).size()
+    frequent_traces = case_counts[case_counts >= threshold].index
+
+    log_filtered = log[log[case_column].isin(frequent_traces)]
+    return log_filtered
+
+
+class Splitter:
+    @staticmethod
+    def split_by_date(log, date="2018"):
+        """
+        Splits the log into two parts based on the given date. Filters all cases that are present in both parts.
+        """
+        date = pd.to_datetime(date).tz_localize('UTC')
+
+        # Split the log
+        before = log[log['time:timestamp'] < date]
+        after = log[log['time:timestamp'] >= date]
+
+        # Find shared 'case:concept:name' values
+        shared_cases = set(before['case:concept:name']).intersection(set(after['case:concept:name']))
+        print(f"Shared cases: {len(shared_cases)}")
+
+        # Filter out rows with shared 'case:concept:name' values
+        before_filtered = before[~before['case:concept:name'].isin(shared_cases)]
+        after_filtered = after[~after['case:concept:name'].isin(shared_cases)]
+
+        return after_filtered, before_filtered
+
+    @staticmethod
+    def split_by_cluster(log, cluster_column='cluster'):
+        """ Splits the log into sublogs based on the cluster column's unique values. """
+        sublogs = {}
+        for label in np.unique(log[cluster_column]):
+            sublogs[label] = log[log[cluster_column] == label]
+
+        sublogs_sorted = dict(reversed(sorted(
+            sublogs.items(), key=lambda item: item[1]['case:concept:name'].nunique()
+        )))
+        return sublogs_sorted
